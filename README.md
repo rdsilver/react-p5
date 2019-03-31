bba72efb25daf10e1d7cdaecce7135a12ed76457
happy_hunter <gherciu553@gmail.com>
2019-03-31 19:57:34 +0300

    setup

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # react-p5
+
+This Component lets you integrate p5 Sketches into your React App.
+
+### Installation
+
+```bash
+npm i react-p5
+```
+
+### Usage
+
+```js
+import React, { Component } from "react";
+import ReactP5 from "react-p5";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            x: 50,
+            y: 50
+        };
+    }
+    setup = (p5, parent) => {
+        p5.createCanvas(500, 500).parent(parent);
+    };
+    draw = p5 => {
+        p5.background(0);
+        p5.ellipse(this.state.x, this.state.y, 70, 70);
+    };
+    windowResized = p => {
+        p.resizeCanvas(200, 200);
+    };
+    incrementX = e => {
+        this.setState(s => ({ x: s.x + 10 }));
+    };
+    render() {
+        return (
+            <div>
+                <h1>My App</h1>
+                <ReactP5 setup={this.setup} draw={this.draw} windowResized={this.windowResized} />
+                <span>X: {this.state.x}</span>
+                <span>Y: {this.state.y}</span>
+                <button onClick={this.incrementX}>X+10</button>
+            </div>
+        );
+    }
+}
+```
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import ReactP5 from "./ReactP5";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            x: 50,
+            y: 50
+        };
+    }
+    setup = (p5, parent) => {
+        p5.createCanvas(500, 500).parent(parent);
+    };
+    draw = p5 => {
+        p5.background(0);
+        p5.ellipse(this.state.x, this.state.y, 70, 70);
+    };
+    windowResized = p => {
+        p.resizeCanvas(200, 200);
+    };
+    incrementX = e => {
+        this.setState(s => ({ x: s.x + 10 }));
+    };
+    incrementY = e => {
+        this.setState(s => ({ y: s.y + 10 }));
+    };
+    decrementX = e => {
+        this.setState(s => ({ x: s.x - 10 }));
+    };
+    decrementY = e => {
+        this.setState(s => ({ y: s.y - 10 }));
+    };
+    render() {
+        return (
+            <div>
+                <center>
+                    <h1>My App</h1>
+                    <ReactP5 setup={this.setup} draw={this.draw} windowResized={this.windowResized} />
+                    <ReactP5 setup={this.setup} draw={this.draw} windowResized={this.windowResized} />
+                    <span>X: {this.state.x}</span>
+                    <span>Y: {this.state.y}</span>
+                    <div>
+                        <button onClick={this.incrementX}>X+10</button>
+                        <button onClick={this.incrementY}>Y+10</button>
+                        <button onClick={this.decrementX}>X-10</button>
+                        <button onClick={this.decrementY}>Y-10</button>
+                    </div>
+                </center>
+            </div>
+        );
+    }
+}
