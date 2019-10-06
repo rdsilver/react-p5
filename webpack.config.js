6c279c8bb132ef6b3db73cddc89c546a7b4ef72e
happy_hunter <gherciu553@gmail.com>
2019-10-06 21:17:14 +0300

    fix: webpack config externals

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,6 @@ module.exports = {
     umdNamedDefine: true,
     globalObject: 'this'
   },
-  externals: {
-    react: 'react',
-    React: "React",
-    react: "React",
-    "window.react": "React",
-    "window.React": "React"
-  },
   module: {
     rules: [
       {
@@ -31,5 +24,24 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js'],
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
   },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
 }
