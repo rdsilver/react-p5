f6ec8c28ea885a9ed6c89c85d169fe91256a4fe5
Gherciu Gheorghe <gherciu553@gmail.com>
2021-03-29 21:45:28 +0300

    test: add some tests

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 import p5 from "p5";
 
-if (typeof window !== "undefined") { window.p5 = p5 };
+// NOTE: assigning p5 to window because someone can need it globally to use in others libraries
+if (typeof window !== "undefined") {
+  window.p5 = p5;
+}
+
+export const p5Events = [
+  "draw",
+  "windowResized",
+  "preload",
+  "mouseClicked",
+  "doubleClicked",
+  "mouseMoved",
+  "mousePressed",
+  "mouseWheel",
+  "mouseDragged",
+  "mouseReleased",
+  "keyPressed",
+  "keyReleased",
+  "keyTyped",
+  "touchStarted",
+  "touchMoved",
+  "touchEnded",
+  "deviceMoved",
+  "deviceTurned",
+  "deviceShaken",
+];
+
 export default class Sketch extends React.Component {
   constructor(props) {
     super(props);
@@ -13,27 +39,7 @@ export default class Sketch extends React.Component {
       p.setup = () => {
         this.props.setup(p, this.canvasParentRef.current);
       };
-      const p5Events = [
-        "draw",
-        "windowResized",
-        "preload",
-        "mouseClicked",
-        "doubleClicked",
-        "mouseMoved",
-        "mousePressed",
-        "mouseWheel",
-        "mouseDragged",
-        "mouseReleased",
-        "keyPressed",
-        "keyReleased",
-        "keyTyped",
-        "touchStarted",
-        "touchMoved",
-        "touchEnded",
-        "deviceMoved",
-        "deviceTurned",
-        "deviceShaken",
-      ];
+
       p5Events.forEach((event) => {
         if (this.props[event]) {
           p[event] = (...rest) => {
--- src/index.test.jsx
+++ src/index.test.jsx
@@ -1,24 +1,63 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { render } from '@testing-library/react';
 import Sketch from './index';
 
 const reactP5TestId = 'react-p5';
 let MockComponent = null;
+let canvas = null;
 
 beforeEach(() => {
-  MockComponent = (...rest) => {
+  MockComponent = (props) => {
     const setup = (p, canvasParentRef) => {
-      p.createCanvas(500, 500).parent(canvasParentRef);
+      canvas = p.createCanvas(500, 500).parent(canvasParentRef);
       p.background(0);
     }
-    return (<Sketch setup={setup} {...rest} />);
+    return (<Sketch setup={setup} {...props} />);
   }
 });
 
 describe('react-p5', () => {
+  it('Should export globally p5 instance', () => {
+    expect(window.p5).toBeDefined();
+  });
+
+  it('Should render correct in SSR mode when window is not-defined', () => {
+    const clonedWindow = global.window;
+    delete global.window;
+
+    const StringComponent = ReactDOMServer.renderToString(<MockComponent />);
+    const StaticComponent = ReactDOMServer.renderToStaticMarkup(<MockComponent />);
+
+    global.window = clonedWindow;
+
+    expect(StringComponent).toMatch(`<div class="${reactP5TestId}" data-testid="${reactP5TestId}" data-reactroot=""></div`);
+    expect(StaticComponent).toMatch(`<div class="${reactP5TestId}" data-testid="${reactP5TestId}"></div`);
+
+    expect(canvas).toBeNull();
+  });
+
+  it('Should render correct in SSR mode when window is defined', () => {
+    const StringComponent = ReactDOMServer.renderToString(<MockComponent />);
+    const StaticComponent = ReactDOMServer.renderToStaticMarkup(<MockComponent />);
+
+    expect(StringComponent).toMatch(`<div class="${reactP5TestId}" data-testid="${reactP5TestId}" data-reactroot=""></div`);
+    expect(StaticComponent).toMatch(`<div class="${reactP5TestId}" data-testid="${reactP5TestId}"></div`);
+    expect(canvas).toBeNull();
+  });
+
+  it('Should call setup function on component mount with corresponding arguments', () => {
+    const setup = jest.fn();
+    render(<MockComponent setup={setup} />);
+
+    expect(setup).toBeCalledTimes(1);
+    expect(setup).toBeCalledWith(expect.any(Object), expect.any(Element));
+  });
+
   it('Should render MockComponent without errors', () => {
     const { getByTestId } = render(<MockComponent />);
 
-    expect(getByTestId(reactP5TestId)).toBeDefined()
+    expect(getByTestId(reactP5TestId)).toBeDefined();
+    expect(canvas).not.toBeNull();
   });
 });
